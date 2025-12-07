4368ce6a482622d1cb7259d16d6a35c576905985
sampath aruna <sampathyt1973@gmail.com>
2025-12-07 10:37:14 +0000

    16:05:14.952 Running build in Washington, D.C., USA (East) – iad1 16:05:

--- src/app/api/genkit/route.ts
+++ src/app/api/genkit/route.ts
@@ -1,17 +1,17 @@
-// import { createNextJSHandler } from '@genkit-ai/next';
-// import { ai } from '@/ai/genkit';
-// import '@/ai/dev';
+import { createNextJSHandler } from '@genkit-ai/next';
+import { ai } from '@/ai/genkit';
+import '@/ai/dev';
 
-// export const { GET, POST } = createNextJSHandler({
-//   ai,
-//   // You can configure your flow authorization and CORS policies here.
-//   // GKE and Cloud Run users should configure CORS via their frontend framework.
-//   // All other users can configure CORS here.
-//   // cors: {
-//   //   origin: '*', // or a more restrictive origin
-//   // },
-//   // auth: async (auth, flow) => {
-//   //   // Implement your authorization logic here.
-//   //   // An empty block means no authorization is required.
-//   // },
-// });
+export const { GET, POST } = createNextJSHandler({
+  ai,
+  // You can configure your flow authorization and CORS policies here.
+  // GKE and Cloud Run users should configure CORS via their frontend framework.
+  // All other users can configure CORS here.
+  // cors: {
+  //   origin: '*', // or a more restrictive origin
+  // },
+  // auth: async (auth, flow) => {
+  //   // Implement your authorization logic here.
+  //   // An empty block means no authorization is required.
+  // },
+});
